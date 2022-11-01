5d577ba7bc437cfe47b5fcc3b1c5334d10213f79
William Abreu <weabreu16@gmail.com>
2022-10-31 23:35:23 -0400

    feat(SectionLabel): className prop added

--- src/components/section-label/SectionLabel.tsx
+++ src/components/section-label/SectionLabel.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import "./SectionLabel.css";
 
 interface Props {
+    className?: string;
     label: string;
     icon: React.ReactNode;
 }
 
 export function SectionLabel(props: Props) {
     return (
-        <div className="section-label">
-            <div className="section-icon">
-                {props.icon}
+        <div className={props.className}>
+            <div className="section-label">
+                <div className="section-icon">
+                    {props.icon}
+                </div>
+                <p className="label">
+                    {props.label}
+                </p>
             </div>
-            <p className="label">
-                {props.label}
-            </p>
         </div>
     )
 }
